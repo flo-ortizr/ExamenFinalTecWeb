cf12bcf4fd758e09a88a585b38aab0536f9e4d07
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-12-19 17:19:20 -0400

    feat: reset password dto

---
+++ Entities/Dtos/Auth/ResetPasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoFinalTecWeb.Entities.Dtos.Auth
+{
+    public class ResetPasswordDto
+    {
+        public string Email { get; init; } = string.Empty;
+        public string newPassword { get; init; } = string.Empty;
+    }
+}
